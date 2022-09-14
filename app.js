2bdb1729e8469db83a612b646bad0677f5377600
Anas <sa464410@gmail.com>
2022-09-14 18:48:01 +0530

    Added SECRET .env variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -5,6 +6,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,8 +19,7 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-const secret = "shaikmohammadanas";
-userSchema.plugin(encrypt, {secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = mongoose.model("User", userSchema);
 
